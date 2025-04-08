a7868c6508dfd27a85fa26fcaf479aaf05c7a7e3
Rutuja Kenjalkar <130883652+rutujakenjalkar@users.noreply.github.com>
2025-04-08 18:20:46 +0530

    Update pythoncode.py

--- pythoncode.py
+++ pythoncode.py
@@ -1,1 +1,7 @@
-print("hello world")
+# Simple Python program to greet the user
+
+# Ask for user's name
+name = input("What's your name? ")
+
+# Print a greeting
+print("Hello, " + name + "! Welcome to Python!")
